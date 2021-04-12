3f086eacb3ad64d4252fda2e36815c938fe2bb7a
Naved Khan <navedkhan0809@gmail.com>
2021-04-13 00:50:22 +0530

    Line comparison UC1 added

--- LineComparisionProblem/src/lineComp/linecomparison.java
+++ LineComparisionProblem/src/lineComp/linecomparison.java
@@ -1,9 +1,33 @@
 package lineComp;
 
+import java.util.Scanner;
+import java.lang.Math;
+
 public class linecomparison {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to line comparison problem");
+		
+		int x1,y1,x2,y2,length;
+		Scanner in = new Scanner(System.in);
+		
+		// Taking co-ordinates from user
+		System.out.println("Enter x1 value: ");
+		x1 = in.nextInt();
+		
+		System.out.println("Enter y1 value: ");
+		y1 = in.nextInt();
+		
+		System.out.println("Enter x2 value: ");
+		x2 = in.nextInt();
+		
+		System.out.println("Enter y2 value: ");
+		y2 = in.nextInt();
+		
+		// Calculating length of line
+		double value = Math.floor(((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)) );
+		
+		System.out.println("The length of line is: ");
+		System.out.println(Math.sqrt(value)); 
 
 	}
 
